b2dfbcae3dce8a1c9e494c98a821472338b8abe0
lghwork <lghwork.@example.com>
2019-02-15 16:23:32 +0800

    index-swiper

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,7 +8,8 @@
       输入城市/景点/游玩主题
     </div>
     <div class="header-right">
-      城市<span class="iconfont arrow-icon">&#xe6aa;</span></div>
+      城市
+      <span class="iconfont arrow-icon">&#xe6aa;</span></div>
   </div>
 </template>
 <script>
